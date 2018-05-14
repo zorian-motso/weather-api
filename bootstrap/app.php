ee642beed301704bfb88aefb0efb0144f690dc60
Zorian <zorianmotso@gmail.com>
2018-05-14 17:39:16 +0300

    add and configure dingo/api package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -78,6 +78,7 @@ $app->singleton(
 |
 */
 
+$app->register(Dingo\Api\Provider\LumenServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
